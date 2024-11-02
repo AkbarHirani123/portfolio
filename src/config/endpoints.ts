1f72616b8f1fe807db514b89021b28f8aa5f21b8
akbar hirani <akbar.hirani123@gmail.com>
2024-11-02 17:02:40 -0400

    Updates

--- src/config/endpoints.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    GET_IP_API: 'https://ipinfo.io/json',
-    WEATHER_API: `https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=2e59a9a713ef7db4c74151871f032d59`,
-    // WEATHER_API: `https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${process.env.WEATHER_API_KEY}`,
-    WEATHER_ICON: 'https://openweathermap.org/img/wn/'
-};
