0e7271970f7a6524302f20eb0a69c4496ea5065c
Akbar Hirani <akbar.hirani123@gmail.com>
2024-11-13 23:04:47 -0500

    Create auto-deploy.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './header';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./header";
 
 function App() {
   return (
